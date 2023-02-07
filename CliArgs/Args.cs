35f6e47b26a7eb8e1ca7c5fb3defab2c85342a3c
Frognar <sebastian.przyszlak@gmail.com>
2023-02-07 18:30:18 +0100

    extract schema validation

--- CliArgs/Args.cs
+++ CliArgs/Args.cs
@@ -12,15 +12,21 @@ public class Args
         if (schema.Length > 0)
         {
             char elementId = schema[0];
-            if (char.IsLetter(elementId) == false)
-                throw new ArgsException(ErrorCode.InvalidArgumentName, elementId);
-            if (schema.Skip(1).Any())
+            string elementTail = schema[1..];
+            ValidateSchemaElementId(elementId);
+            if (elementTail.Any())
                 throw new ArgsException(ErrorCode.InvalidArgumentFormat, elementId);
 
             Count = 1;
         }
         else if (args.Length > 0)
             throw new ArgsException(ErrorCode.UnexpectedArgument, args[0][1]);
+    }
+
+    static void ValidateSchemaElementId(char elementId)
+    {
+        if (char.IsLetter(elementId) == false)
+            throw new ArgsException(ErrorCode.InvalidArgumentName, elementId);
     }
 
     public bool GetBoolean(char elementId)
