5956170873e5f077ae6a6ac982de318861555d94
Frognar <sebastian.przyszlak@gmail.com>
2023-02-07 20:04:30 +0100

    Allow set ErrorArgumentId later

--- CliArgs/Exceptions/ArgsException.cs
+++ CliArgs/Exceptions/ArgsException.cs
@@ -5,11 +5,21 @@ namespace Frognar.CliArgs.Exceptions;
 public class ArgsException : Exception
 {
     public ErrorCode ErrorCode { get; }
-    public char ErrorArgumentId { get; }
+    public char ErrorArgumentId { get; private set; }
     
     public ArgsException(ErrorCode errorCode, char errorArgumentId)
     {
         ErrorCode = errorCode;
         ErrorArgumentId = errorArgumentId;
+    }
+    
+    public ArgsException(ErrorCode errorCode)
+    {
+        ErrorCode = errorCode;
+    }
+
+    public void SetErrorArgumentId(char elementId)
+    {
+        ErrorArgumentId = elementId;
     }
 }
